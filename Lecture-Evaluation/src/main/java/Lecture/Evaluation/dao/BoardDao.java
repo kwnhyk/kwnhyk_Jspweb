f5099356334e51cb0d2d0d7b5677045c939f9c18
kwnhyk <kwnhyk@naver.com>
2020-08-04 02:27:23 +0900

    자유게시판View제외완성MVC017

--- Lecture-Evaluation/src/main/java/Lecture/Evaluation/dao/BoardDao.java
+++ Lecture-Evaluation/src/main/java/Lecture/Evaluation/dao/BoardDao.java
@@ -2,16 +2,17 @@ package Lecture.Evaluation.dao;
 
 import java.util.List;
 
+
 import Lecture.Evaluation.domain.BoardVO;
 
 public interface BoardDao {
 
 	
 	public void create(BoardVO vo) throws Exception;
-	public  BoardVO read(BoardVO vo) throws Exception;
+	public  BoardVO read(int bno) throws Exception;
 	public void update(BoardVO vo) throws Exception;
-	public void delete(BoardVO vo) throws Exception;
-	public void increaseViewcnt(BoardVO vo) throws Exception;
-	public List<BoardVO> listALl() throws Exception;
+	public void delete(int bno) throws Exception;
+	public void increaseViewcnt(int bno) throws Exception;
+	public List<BoardVO> listAll() throws Exception;
 
 }
--- Lecture-Evaluation/src/main/java/Lecture/Evaluation/web/BoardController.java
+++ Lecture-Evaluation/src/main/java/Lecture/Evaluation/web/BoardController.java
@@ -1,15 +1,73 @@
 package Lecture.Evaluation.web;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import Lecture.Evaluation.domain.BoardVO;
+import Lecture.Evaluation.service.BoardService;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
 	
 	
+	@Autowired
+	BoardService boardService;
 	
 	
-	@RequestMapping("insert")
-	public
+	@RequestMapping("list")
+	public ModelAndView list() throws Exception{
+		List<BoardVO> list = boardService.listAll();
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("board/list");
+		mav.addObject("list",list);
+		return mav;
+		
+	
+	}
+	@GetMapping("write")
+	public String write() {
+		return "board/write";
+	}
+	@PostMapping("insert")
+	public String insert(@ModelAttribute BoardVO vo)  throws Exception{
+		
+		boardService.create(vo);
+		return "redirect:list";
+	}
+	@GetMapping("view")
+	public ModelAndView view(@RequestParam int bno,HttpSession session)throws Exception{
+		
+		
+	boardService.increaseViewcnt(bno, session);
+	ModelAndView mav = new ModelAndView();
+	mav.setViewName("board/view");
+	mav.addObject("dto",boardService.read(bno));
+	return mav;
+	}
+	@PostMapping("update")
+	public String update(@ModelAttribute BoardVO vo) throws Exception{
+		boardService.update(vo);
+		return "redirect:list";
+	}
+	@RequestMapping("delete")
+	public String delete(@RequestParam int bno) throws Exception{
+		boardService.delete(bno);
+		return "reddirect:list";
+	}
+
+
+	
+	
+	
 }
